54ea046a84cfa55273eecf7d41252f514f8d070a
eliasmarin2390 <eamarin@uamv.edu.ni>
2025-04-23 13:12:42 -0600

    He añadido el primer ejercicio

---
+++ ejercicio1/ejercicio1.py
@@ -0,0 +1,3 @@
+# ejercicio 1: Una escuela de educación primaria requiere un algoritmo que muestre los datos de losestudiantes de un salón de clase ordenados de forma ascendente, según un parámetroindicado; este parámetro puede ser cualquiera de los siguientes campos: carnet, nombres,apellidos, peso, estatura, sexo, promedio.
+
+
